1fcf515656d1bca3e19c0769359395cdf2b17143
Nicholas <nicholasbrown.us@gmail.com>
2023-01-26 13:38:24 -0500

    3 Dynamic js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+alert("You must be over 14 to purchase or ride a motorcycle");
+
+let userName = prompt("Welcome to Top Performance Motors! What is your name?");
+
+if (confirm("Are you a beginning or new rider?")) {
+    document.write ("Welcome to a great hobby! "+ userName);
+    
+} else {
+    document.write("Glad you are continuing your adventure with us " + userName)
+}
+
+
+
+//document.write
+//prompt
+//alert
