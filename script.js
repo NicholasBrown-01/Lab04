db2f714c6c77fd79087cbee8f5e0d8ecffb68013
Nicholas <nicholasbrown.us@gmail.com>
2023-02-05 20:55:15 -0500

    Changes to make more professional and cohesive

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
 
-//THIS IS MY ALERT
+// //THIS IS MY ALERT
 
-alert("You must be over the age of 14 to purchase or operate a motorcycle");
-
-
-// THIS IS MY FUNCTION PROMPT
-
-function ageCheck() {
-    let age = prompt("Are you over the age of 14? Yes or No");
-
-    if (age) {
-        while (age.toLowerCase() !== "yes") {
-            ageCheck();
-            console.log(age);
-            return age;
-        }
-    } else { 
-        ageCheck();
-    }
-}
-    ageCheck();
-
-// THIS IS MY GREETING FUNCTION
-
-    function userGreeting() {
-        userName = prompt("Welcome to Top Performance Motors! What is your name?");
-        console.log(userName);
-        document.write("Thank you for shopping with us today " + userName);
-        return userName;
-    }
-
-    userGreeting();
+// alert("You must be over the age of 14 to purchase or operate a motorcycle");
 
 
+// // THIS IS MY FUNCTION PROMPT
+
+// function ageCheck() {
+//     let age = prompt("Are you over the age of 14? Yes or No");
+
+//     if (age) {
+//         while (age.toLowerCase() !== "yes") {
+//             ageCheck();
+//             console.log(age);
+//             return age;
+//         }
+//     } else { 
+//         ageCheck();
+//     }
+// }
+//     ageCheck();
+
+// // THIS IS MY GREETING FUNCTION
+
+//     function userGreeting() {
+//         userName = prompt("Welcome to Top Performance Motors! What is your name?");
+//         console.log(userName);
+//         document.write("Thank you for shopping with us today " + userName);
+//         return userName;
+//     }
+
+//     userGreeting();
 
 
-// //CREATING THE FUNCTION FOR DISPLAYING PICTURES
-function popupPicture() {
-    let number = prompt("Pick a number between 1 and 3");
-    console.log(number);
 
-    if (number == 1 || number == 2 || number == 3) {
-        console.log(number);
-    } else {
-        popupPicture();
-    }
 
-    for (let i = 0; i < number; i++) {
-        document.write("<img src='./streetbike.jpg' width = '350px'></img>");
-        console.log(number);
-        console.log(i);
-    }
-}
+// // //CREATING THE FUNCTION FOR DISPLAYING PICTURES
+// function popupPicture() {
+//     let number = prompt("Pick a number between 1 and 3");
+//     console.log(number);
 
-popupPicture();
+//     if (number == 1 || number == 2 || number == 3) {
+//         console.log(number);
+//     } else {
+//         popupPicture();
+//     }
+
+//     for (let i = 0; i < number; i++) {
+//         document.write("<img src='./streetbike.jpg' width = '350px'></img>");
+//         console.log(number);
+//         console.log(i);
+//     }
+// }
+
+// popupPicture();
 
     //     if (number == 1) {
     //         document.write("<img src='./streetbike.jpg' width = '350px'></img>");
@@ -76,3 +76,4 @@ popupPicture();
     //     }
         
     // }
+
